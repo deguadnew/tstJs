8c7a619b427ca69b67c019d4e4e6b04491bb5cec
adnewdegu <deguadnewdilu@gmail.com>
2023-01-24 22:11:59 +0100

    adding function and json expected

--- librarySet.js
+++ librarySet.js
@@ -1,6 +1,20 @@
 function checkJsonEquality(actualJson, expectedJson){
-    if(_.isEqual(actualJson,expectedJson) == true)
+    if(_.isEqual(actualJson,expectedJson))
     return true;
     else 
+    return false;
+}
+function checkingStatusCode(actualJson){
+    if(actualJson.code == 200)
+    return true;
+    else
+    return false;
+}
+
+function checkSubtrings(actualJson, targetString){
+    if(actualJson.includes(targetString)){
+        return true;
+    }
+    else
     return false;
 }
